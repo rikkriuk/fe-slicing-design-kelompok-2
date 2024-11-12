9879e4754953e8009be523c89ef0b4026a45a3ec
ikokywu <rikiridwan949@gmail.com>
2024-11-12 21:13:39 +0700

    fix: folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import HomeContainer from './containers/HomeContainer';
 import AboutContainer from './containers/AboutContainer';
 import ServicesContainer from './containers/ServicesContainer';
 import PortfolioContainer from './containers/PortfolioContainer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomeContainer />} />
-        <Route path="/about" element={<AboutContainer />} />
-        <Route path="/services" element={<ServicesContainer />} />
-        <Route path="/portfolio" element={<PortfolioContainer />} />
+        <Route path='/' element={<HomeContainer />} />
+        <Route path='/about' element={<AboutContainer />} />
+        <Route path='/services' element={<ServicesContainer />} />
+        <Route path='/portfolio' element={<PortfolioContainer />} />
       </Routes>
     </Router>
   );
