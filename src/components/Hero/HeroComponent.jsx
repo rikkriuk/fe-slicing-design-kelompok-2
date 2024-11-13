abf92e9d056f83a93fe7276b880815ff796ce18a
ikokywu <rikiridwan949@gmail.com>
2024-11-13 07:07:15 +0700

    feat: add responsive styling for HeroComponent, NavbarComponent and FooterComponent layout

--- src/components/Hero/HeroComponent.jsx
+++ src/components/Hero/HeroComponent.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import './HeroStyles.css';
-import HeroImage from '../../assets/hero-image.png'
+import React from "react";
+import "./HeroStyles.css";
+import HeroImage from "../../assets/hero-image.png"
 
-const Hero = () => {
+const HeroComponent = () => {
    return (
-      <section className='container my-5'>
-         <div className='container d-flex align-items-center justify-content-between'>
-            <div>
-               <h3 className='text-main-color'>\ We Are Here \</h3>
-               <h2 className='h2-custom my-4'>Better Insights For <br /> Business Growth</h2>
-               <button className='btn-view-more'>View More</button>
+      <section className="container my-5">
+         <div className="container row align-items-center gap-5 gap-lg-0">
+            <div className="col-12 order-2 order-lg-2 md-6 col-md-12 col-lg-6">
+               <h3 className="text-main-color fs-3 fs-md-5">\ We Are Here \</h3>
+               <h2 className="h2-custom my-4">Better Insights For <br /> Business Growth</h2>
+               <button className="btn-view-more">View More</button>
             </div>
 
-            <div>
-               <img src={HeroImage} alt="hero-image" />
+            <div className="col-12 order-1 order-lg-2 md-6 col-md-12 col-lg-6">
+               <img src={HeroImage} alt="hero-image" className="img-fluid" />
             </div>
-
          </div>
       </section>
    )
 }
 
-export default Hero;
+export default HeroComponent;
--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -1,31 +1,46 @@
-import React from 'react';
-import './NavbarStyles.css';
-import { Link } from 'react-router-dom';
-import imagePath from '../../assets/logo.png';
+import React, { useState } from "react";
+import "./NavbarStyles.css";
+import { Link } from "react-router-dom";
+import LogoImage from "../../assets/logo.png";
 
-const Navbar = () => {
+const NavbarComponent = () => {
+   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+   // Function to open/close the navbar in small screens
+   const toggleNavbar = () => {
+      setIsNavbarOpen(!isNavbarOpen);
+   };
+
    return (
-      <header className='container-fluid py-4 shadow-sm'>
-         <div className='container d-flex justify-content-between'>
-            <div className='d-flex align-items-center gap-3'>
-               <img src={imagePath} alt='logo-image' height={35} />
-               <h1 className='fs-3 fw-bold m-0'>Logoipsum</h1>
+      <header className="container-fluid py-4 shadow-sm">
+         <div className="container d-flex justify-content-between">
+            <div className="d-flex align-items-center gap-3">
+               <img src={LogoImage} alt="logo-image" height={35} />
+               <Link to={"/"} className="fs-3 fw-bold text-decoration-none text-second-color m-0">Logoipsum</Link>
             </div>
 
-            <nav className='d-flex align-items-center gap-5'>
-               <ul className='d-flex align-items-center gap-4 list-unstyled m-0'>
-                  <li><Link className='text-decoration-none text-color fw-bold' to={'/about'}>About</Link></li>
-                  <li><Link className='text-decoration-none text-color fw-bold' to={'/our-services'}>Our Services</Link></li>
-                  <li><Link className='text-decoration-none text-color fw-bold' to={'/portfolio'}>Portfolio</Link></li>
-                  <li><Link className='text-decoration-none text-color fw-bold' to={'/blog'}>Blog</Link></li>
-                  <li><Link className='text-decoration-none text-color fw-bold' to={'/contact'}>Contact</Link></li>
+            <button onClick={toggleNavbar} className="btn d-lg-none fs-2"><i className="bi bi-list"></i></button>
+
+            {isNavbarOpen && <div className="blur-overlay" onClick={toggleNavbar}></div>}
+
+            <nav className={`d-flex align-items-center gap-5 nav-small-screen rounded-0 ${isNavbarOpen ? "show" : ""}`}>
+               <div className="position-absolute d-lg-none top-0 d-flex justify-content-end w-100 mt-3 me-5">
+                  <button onClick={toggleNavbar} className="btn text-white fs-2"><i className="bi bi-x-lg"></i></button>
+               </div>
+
+               <ul className="d-flex align-items-center gap-4 list-unstyled m-0">
+                  <li><Link className="text-decoration-none text-second-color fw-semibold" to={"/about"}>About</Link></li>
+                  <li><Link className="text-decoration-none text-second-color fw-semibold" to={"/our-services"}>Our Services</Link></li>
+                  <li><Link className="text-decoration-none text-second-color fw-semibold" to={"/portfolio"}>Portfolio</Link></li>
+                  <li><Link className="text-decoration-none text-second-color fw-semibold" to={"/blog"}>Blog</Link></li>
+                  <li><Link className="text-decoration-none text-second-color fw-semibold" to={"/contact"}>Contact</Link></li>
                </ul>
 
-               <button className='btn-custom'>Get In Touch</button>
+               <button className="btn-custom">Get In Touch</button>
             </nav>
          </div>
       </header>
     )
 }
 
-export default Navbar;
+export default NavbarComponent;
