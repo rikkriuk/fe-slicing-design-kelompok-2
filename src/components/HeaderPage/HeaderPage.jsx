f839ea92038e69f6c75ddfd4204ac0e64a25991e
Fauzi Ferdiansyah <43751540+fauziferdi@users.noreply.github.com>
2024-11-13 09:14:14 +0700

    feat: add page header components

---
+++ src/components/HeaderPage/HeaderPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./HeaderPageStyles.css";
+
+const HeaderPage = ({ page }) => {
+  return (
+    <div className="container-fluid bg-header-page">
+      <div className="container ">
+        <div class="d-flex justify-content-center text-center pd-custom-page">
+          <div>
+            <h1 className="color-primary-header fw-bold">{page}</h1>
+            <h3 className="text-white">Home / {page}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderPage;
