1b430de970736b47bdd30f81ac470f857f6bfc46
ikokywu <rikiridwan949@gmail.com>
2024-11-13 22:24:55 +0700

    feat: add component into blogContainer

--- src/containers/BlogsContainer.jsx
+++ src/containers/BlogsContainer.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import BlogTopComponent from "../components/BlogTop/BlogTopComponent";
 import BlogBottomComponent from "../components/BlogBottom/BlogBottomComponent";
 import TitlePageComponent from "../components/TitlePage/TitlePageComponent";
+import AboutComponent from "../components/About/AboutComponent";
+
 const BlogsContainer = () => {
   return (
     <div>
+      <AboutComponent title="Blogs" subtitle="Home / Blogs" />
       <TitlePageComponent title={"Our Blog"} description={"Latest Post"} />
       <BlogTopComponent />
       <BlogBottomComponent />
