c79e92138f09fd07241bc579bb201b4e281071c2
feryokta16 <110111575+feryokta16@users.noreply.github.com>
2024-11-13 09:00:47 +0700

    feat add about Component
    about us component, card Component

---
+++ src/components/Card/CardComponent.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import person from "../../assets/Person1.jpeg";
+import person2 from "../../assets/Person2.png";
+import person3 from "../../assets/Person3.jpeg";
+import person4 from "../../assets/Person4.jpeg";
+import "./CardStyles.css";
+
+const CardComponent = () => {
+  // data for Card Component
+  const data = [
+    { name: "Larry F. Burnett", jabatan: "CEO", image: person },
+    { name: "Meghan J. Webb", jabatan: "CTO", image: person2 },
+    { name: "Yvonne J. Cullum", jabatan: "COO", image: person3 },
+    { name: "Diana H. Williams", jabatan: "CEO", image: person4 },
+    { name: "Larry F. Burnett", jabatan: "CEO", image: person },
+    { name: "Meghan J. Webb", jabatan: "CTO", image: person2 },
+    { name: "Yvonne J. Cullum", jabatan: "COO", image: person3 },
+    { name: "Diana H. Williams", jabatan: "CEO", image: person4 },
+  ];
+  return (
+    // section card for About Component
+    <section className="section-card container">
+      <h4>\Team\</h4>
+      <h1>Our Team</h1>
+      <div className="row">
+        {data &&
+          data.map((item, index) => (
+            <div className="col-md-3" key={index}>
+              <div className="card container">
+                <div className="card-body">
+                  <img
+                    src={item.image}
+                    alt="person"
+                    className="img-fluid rounded"
+                  />
+                  <h6>{item.name}</h6>
+                  <p>{item.jabatan}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </section>
+  );
+};
+
+export default CardComponent;
--- src/containers/AboutContainer.jsx
+++ src/containers/AboutContainer.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import About from "../components/About/AboutComponent";
+import AboutUS from "../components/AboutUs.jsx/AboutUsComponent";
+import Card from "../components/Card/CardComponent";
 
 const AboutContainer = () => {
-   return (
-        <div>
-            <h1>About Container</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <About title="About US" subtitle="Home/About" />
+      <AboutUS />
+      <Card />
+    </div>
+  );
+};
 
 export default AboutContainer;
